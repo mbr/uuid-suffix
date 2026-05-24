611576dfda890b983830104ce6055d202f46611a
Marc Brinkmann <git@marcbrinkmann.de>
2026-05-24 23:05:40 +0200

    Check with `--all-features`

--- check.sh
+++ check.sh
@@ -7,6 +7,6 @@ set -e
 echo "rustc $(rustc --version) at $(which rustc), cargo $(cargo --version) at $(which cargo)"
 
 ./format.sh --check
-RUSTFLAGS="-D warnings" cargo check
+RUSTFLAGS="-D warnings" cargo check --all-features
 cargo clippy -- -D warnings
 cargo test
